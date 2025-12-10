1de13ac02512fd21d60cc662809931e02e16aad0
FelixAkabati <129680272+FelixAkabati007@users.noreply.github.com>
2025-12-10 09:01:19 -1200

    Implemented vercel Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,10 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { ErrorBoundary } from "./components/ErrorBoundary";
+import { inject } from "@vercel/analytics";
 
 const rootEl = document.getElementById("root")!;
+inject();
 createRoot(rootEl).render(
   <React.StrictMode>
     <ErrorBoundary>
